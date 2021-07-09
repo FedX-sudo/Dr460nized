aeaac23436e93493a21483306f75760472bfc6b3
fedx <fedx-sudo@pm.me>
2021-07-09 14:23:30 -0600

    small changes

--- dntos/shell/commands.py
+++ dntos/shell/commands.py
@@ -3,8 +3,18 @@ import logging
 import toml
 import os
 
-with open("/home/fedx/Desktop/Code/Dr460nized/dntos/shell/config.toml", "r") as f:
+with open(os.path.realpath("dntos/shell/config.toml"), "r") as f:
     fs = toml.load(f)
 
 def pwd(args):
   return fs["workingdir"]
+
+def ls(args):
+  if args["path"] == "":
+    path = fs["workingdir"]
+  elif args["path"] == "~":
+    path = "Muffin_Man"
+  try:
+    return(fs[path])
+  except:
+    return("Error, no such file or directory")
