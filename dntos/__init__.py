c93e177f2d6dc98161d98b0cef91c1a652823b06
fedx <fedx-sudo@pm.me>
2021-07-19 12:01:08 -0600

    changed some stuffs

--- dntos/__init__.py
+++ dntos/__init__.py
@@ -5,33 +5,122 @@ import os
 from pynput.keyboard import Key, Listener
 
 import dntos.shell
-import ui
+import dntos.ui
 
 pygame.init()
 
-# VVVVVVVVVVV importing image assets. VVVVVVVVVVVVVVVVVV
-screen = pygame.display.set_mode([1280, 720])
+class game():
+  def __init__(self):
+    self.screen = pygame.display.set_mode([1280, 720])
+    self.donut_img = pygame.image.load('dntos/ui/resources/donut.png')
+    self.donut_img = pygame.transform.smoothscale(self.donut_img, (50, 50))
+    self.donut = self.screen.blit(self.donut_img, [10, 665])
+
+    self.chrome_img = pygame.image.load('dntos/ui/resources/chrome.png') # chrome.png is of no relation to Google or any Google patents relating to Chrome. <_<
+    self.chrome_img = pygame.transform.smoothscale(self.chrome_img, (50, 50))
+    self.chrome = self.screen.blit(self.chrome_img, [70, 665]) # it is pure coincidence that all the internal variables for our browser thing have the same name as the most popular browser in the world, no correlation whatsoever.
+
+    self.file_explorer_img = pygame.image.load('dntos/ui/resources/file_explorer.png')
+    self.file_explorer_img = pygame.transform.smoothscale(self.file_explorer_img, (50, 50))
+    self.file_explorer = self.screen.blit(self.file_explorer_img, [130, 665])
+
+    self.console_img = pygame.image.load('dntos/ui/resources/console.png')
+    self.console_img = pygame.transform.smoothscale(self.console_img, (50, 50))
+    self.console = self.screen.blit(self.console_img, [190, 665])
+
+    self.x_quit_img = pygame.image.load('dntos/ui/resources/x.png')
+    self.x_quit_img = pygame.transform.smoothscale(self.x_quit_img, (30, 30))
+
+    self.hacker_img = pygame.image.load('dntos/ui/resources/hacker.png')
+    self.hacker_img = pygame.transform.smoothscale(self.hacker_img, (200, 200))
+
+    self.training_img = pygame.image.load('dntos/ui/resources/training.png')
+    self.training_img = pygame.transform.smoothscale(self.training_img, (200, 200))
+
+    self.img_1 = pygame.image.load('dntos/ui/resources/1.png')
+    self.img_1 = pygame.transform.smoothscale(self.img_1, (100, 100))
+    self.img_1_b = self.screen.blit(self.img_1, [100, 100])
+
+
+    self.clock = pygame.time.Clock()
+    self.font = pygame.font.SysFont(None, 40)
+    self.hacker_b = self.screen.blit(self.hacker_img, [300, 300])
+    self.training_b = self.screen.blit(self.training_img, [900, 300])
+
+
+    self.draw_console = False
+    self.draw_donut = False# A random variable which does nothing. Don't delete it!
+    self.draw_file_explorer = False# A random variable which does nothing. Don't delete it!
+    self.draw_chrome = False # A random variable which does nothing. Don't delete it! Oh and I almost forgot, this is of no association with Google, and its purely coincidence its called chrome.
+
+
+    self.console_text= "Muffin-Man>"
+    self.console_output_text = ""
+
+
+  def console(self):
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 40, 1280, 620))
+    pygame.draw.rect(screen, (220, 220, 220), pygame.Rect(0, 0, 1280, 40))
+    self.x_quit = screen.blit(x_quit_img, [1230, 5])
+    self.console_text_r = font.render(console_text, True, (255, 255, 255))
+    self.screen.blit(console_text_r, [10, 50])
+
+    self.console_output_text_r = font.render(console_output_text, True, (255, 255, 255))
+    self.screen.blit(console_output_text_r, [10, 100])
+
+
+    pygame.display.flip() # This does something, what I don't know.
+  def reset_screen(self): # This is a function which closes all the windows.
+    self.draw_console = False
+    self.draw_donut = False# A random variable which does nothing. Don't delete it!
+    self.draw_file_explorer = False# A random variable which does nothing. Don't delete it!
+    self.draw_chrome = False # A random variable which does nothing. Don't delete it! Oh and I almost forgot, this is of no association with Google, and its purely coincidence its called chrome.
+
+  def training_level_1(self):
+      print("yo")
+      self.screen.fill((50, 82, 123)) # background
+
+      pygame.draw.rect(self.screen, (10, 10, 10), pygame.Rect(0, 660, 1280, 60)) # taskbar
+
+      self.screen.blit(self.donut_img, [10, 665]) # blit? blit! What does that mean?!
+      self.screen.blit(self.chrome_img, [70, 665])
+      self.screen.blit(self.file_explorer_img, [130, 665])
+      self.screen.blit(self.console_img, [190, 665])
+
+      if (self.draw_console):
+          self.console()
+
+      for evetn in event_list: # um, what?!
+        if event.type == pygame.MOUSEBUTTONDOWN:
+              print("yo #2")
+              mouse_pos = pygame.mouse.get_pos()
+              try: # this is needed here because x is not always initialized
+                  if (x_quit.collidepoint(mouse_pos)):
+                      reset_screen()
+              except:
+                  pass
+              if (self.donut.collidepoint(mouse_pos)):
+                  self.reset_screen()
+                  self.draw_donut = True
+              if (self.chrome.collidepoint(mouse_pos)):
+                  self.reset_screen()
+                  self.draw_chrome = True
+              if (self.file_explorer.collidepoint(mouse_pos)):
+                  self.reset_screen()
+                  self.draw_file_explorer = True
+              if (self.console.collidepoint(mouse_pos)):
+                  self.reset_screen()
+                  self.draw_console = True
+
+args=game()
+donut = args.screen.blit(args.donut_img, [10, 665])
+
 pygame.display.set_caption("Donut OS emulator")
 
-clock = pygame.time.Clock()
 
-
-ui.img_import(screen)
-
-
-font = pygame.font.SysFont(None, 40)
 
 # This just a tad bit of house keeping to allow the console to work.
-global console_text
-console_text = "Muffin-Man>"
-global console_output_text
-console_output_text = ""
 
-# The following variables define weather the apps should be drawn or not.
-draw_console = False
-draw_donut = False# A random variable which does nothing. Don't delete it!
-draw_file_explorer = False# A random variable which does nothing. Don't delete it!
-draw_chrome = False # A random variable which does nothing. Don't delete it! Oh and I almost forgot, this is of no association with Google, and its purely coincidence its called chrome.
 
 def reset_screen(): # This is a function which closes all the windows.
     draw_console = False
@@ -70,123 +159,6 @@ t1  = threading.Thread(target=listen_for_keys) # a new thread it needed because 
 t1.start()
 
 
-
-
-def training_route(lesson_number): # this is the training route
-    if(lesson_number == 1):
-
-        screen.fill((50, 82, 123)) # background
-
-        pygame.draw.rect(screen, (10, 10, 10), pygame.Rect(0, 660, 1280, 60)) # taskbar
-
-        screen.blit(donut_img, [10, 665]) # blit? blit! What does that mean?!
-        screen.blit(chrome_img, [70, 665])
-        screen.blit(file_explorer_img, [130, 665])
-        screen.blit(console_img, [190, 665])
-
-        global draw_console
-        if (draw_console == True):
-            ui.Console(screen)
-
-        pygame.display.flip() # This does something, what I don't know.
-
-        for evetn in event_list: # um, what?!
-          if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                try: # this is needed here because x is not always initialized
-                    if (x_quit.collidepoint(mouse_pos)):
-                        reset_screen()
-                except:
-                    pass
-                if (donut.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_donut = True
-                if (chrome.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_chrome = True
-                if (file_explorer.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_file_explorer = True
-                if (console.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_console = True
-    elif(lesson_number == 2):
-        print("training_route_2")
-    elif(lesson_number == 3):
-        print("training_route_3")
-    elif(lesson_number == 4):
-        print("training_route_4")
-    elif(lesson_number == 5):
-        print("training_route_5")
-    elif(lesson_number == 6):
-        print("training_route_6")
-    elif(lesson_number == 7):
-        print("training_route_7")
-    elif(lesson_number == 8):
-        print("training_route_8")
-
-def hacker_route(lesson_number): # this is the hacker route
-    if(lesson_number == 1):
-
-
-        screen.fill((50, 82, 123)) # background
-
-        pygame.draw.rect(screen, (10, 10, 10), pygame.Rect(0, 660, 1280, 60)) # taskbar
-
-        screen.blit(donut_img, [10, 665]) # blit? blit! What does that mean?!
-        screen.blit(chrome_img, [70, 665])
-        screen.blit(file_explorer_img, [130, 665])
-        screen.blit(console_img, [190, 665])
-
-        global draw_console
-        if (draw_console == True):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 40, 1280, 620))
-            pygame.draw.rect(screen, (220, 220, 220), pygame.Rect(0, 0, 1280, 40))
-            x_quit = screen.blit(x_quit_img, [1230, 5])
-            console_text_r = font.render(console_text, True, (255, 255, 255))
-            screen.blit(console_text_r, [10, 50])
-
-            console_output_text_r = font.render(console_output_text, True, (255, 255, 255))
-            screen.blit(console_output_text_r, [10, 100])
-
-
-        pygame.display.flip() # This does something, what I don't know.
-
-        for evetn in event_list: # um, what?!
-          if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                try: # this is needed here because x is not always initialized
-                    if (x_quit.collidepoint(mouse_pos)):
-                        reset_screen()
-                except:
-                    pass
-                if (donut.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_donut = True
-                if (chrome.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_chrome = True
-                if (file_explorer.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_file_explorer = True
-                if (console.collidepoint(mouse_pos)):
-                    reset_screen()
-                    draw_console = True
-    elif(lesson_number == 2):
-        print("hacker_route_2")
-    elif(lesson_number == 3):
-        print("hacker_route_3")
-    elif(lesson_number == 4):
-        print("hacker_route_4")
-    elif(lesson_number == 5):
-        print("hacker_route_5")
-    elif(lesson_number == 6):
-        print("hacker_route_6")
-    elif(lesson_number == 7):
-        print("hacker_route_7")
-    elif(lesson_number == 8):
-        print("hacker_route_8")
-
 global path_selected
 global level_selected
 global path
@@ -200,23 +172,25 @@ level_number = 0
 
 run = True
 while run:
-    clock.tick(60)
+    args.clock.tick(60)
     event_list = pygame.event.get()
     for event in event_list:
         if event.type == pygame.QUIT:
             run = False
 
-    screen.fill((50, 82, 123))
+    args.screen.fill((50, 82, 123))
 
     if (path_selected == False):
-        screen.blit(hacker_img, [300, 300])
-        screen.blit(training_img, [900, 300])
+
+        args.screen.blit(args.hacker_img, [300, 300])
+
+        args.screen.blit(args.training_img, [900, 300])
 
     if (level_selected == False):
         # print("draw some hacker stuff")
-        screen.blit(img_1, [100, 100])
+        args.screen.blit(args.img_1, [100, 100])
 
-    if(play_level == True): # after selected is will start the level
+    if(play_level): # after selected is will start the level
         if(path == "T"):
             training_route(level_number)
         if(path == "H"):
@@ -227,22 +201,20 @@ while run:
     for evetn in event_list:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            if (hacker_b.collidepoint(mouse_pos)):
-                print("hacker clicked")
+            if (args.hacker_b.collidepoint(mouse_pos)):
                 path_selected = True
                 level_selected = False
                 path = "H" # makes the path to hacker
-            if (training_b.collidepoint(mouse_pos)):
-                print("training clicked")
+            if (args.training_b.collidepoint(mouse_pos)):
                 path_selected = True
                 level_selected = False
                 path = "T" # makes the path to training
-            try:
-                if (img_1_b.collidepoint(mouse_pos)):
-                    level_number = 1
-                    play_level = True
-            except:
-                pass
+            #try:
+                #if (args.img_1_b.collidepoint(mouse_pos)):
+            args.training_level_1()
+
+            #except:
+                #print("dang")
 
 pygame.quit()
 os._exit(0)
--- dntos/ui/__init__.py
+++ dntos/ui/__init__.py
@@ -1,45 +1,55 @@
+#!/usr/bin/env python3
 import pygame
 # UI Elements such as applications
 # This is a small chunk of code which will define the donut
-def img_import(screen):
-    donut_img = pygame.image.load('resources/donut.png')
+def img_import(args):
+    screen = args["screen"]
+    out = {}
+    donut_img = pygame.image.load('dntos/ui/resources/donut.png')
     donut_img = pygame.transform.smoothscale(donut_img, (50, 50))
-    donut = screen.blit(donut_img, [10, 665])
+    out:"donut_img" = donut_img
+    #donut = screen.blit(donut_img, [10, 665])
 
     # This small bit of code is defining the C0m0d0 Dr4g0n icon, which id definitely not the Google Chrome icon, or named after Comodo Dragon Browser.
-    chrome_img = pygame.image.load('resources/chrome.png') # chrome.png is of no relation to Google or any Google patents relating to Chrome. <_<
+    chrome_img = pygame.image.load('dntos/ui/resources/chrome.png') # chrome.png is of no relation to Google or any Google patents relating to Chrome. <_<
     chrome_img = pygame.transform.smoothscale(chrome_img, (50, 50))
-    chrome = screen.blit(chrome_img, [70, 665]) # it is pure coincidence that all the internal variables for our browser thing have the same name as the most popular browser in the world, no correlation whatsoever.
+    out:"donut_img" = chrome_img
+    #chrome = screen.blit(chrome_img, [70, 665]) # it is pure coincidence that all the internal variables for our browser thing have the same name as the most popular browser in the world, no correlation whatsoever.
 
     # At this point I think you get the point, this is defining out file browser picture...
-    file_explorer_img = pygame.image.load('resources/file_explorer.png')
+    file_explorer_img = pygame.image.load('dntos/ui/resources/file_explorer.png')
     file_explorer_img = pygame.transform.smoothscale(file_explorer_img, (50, 50))
-    file_explorer = screen.blit(file_explorer_img, [130, 665])
+    out:"file_explorer_img" = file_explorer_img
+    #file_explorer = screen.blit(file_explorer_img, [130, 665])
 
     # This is defining the Unix terminal emulator icon, which is the only app you need.
-    console_img = pygame.image.load('resources/console.png')
+    console_img = pygame.image.load('dntos/ui/resources/console.png')
     console_img = pygame.transform.smoothscale(console_img, (50, 50))
-    console = screen.blit(console_img, [190, 665])
+    out:"console_img" = console_img
+    #console = screen.blit(console_img, [190, 665])
 
     # quit out of window symbol ( X )
-    x_quit_img = pygame.image.load('resources/x.png')
+    x_quit_img = pygame.image.load('dntos/ui/resources/x.png')
     x_quit_img = pygame.transform.smoothscale(x_quit_img, (30, 30))
+    out:"x_quit_img" = x_quit_img
 
     # This defines an image depicting an anonymous hacker representing the hacker route.
-    hacker_img = pygame.image.load('resources/hacker.png')
+    hacker_img = pygame.image.load('dntos/ui/resources/hacker.png')
     hacker_img = pygame.transform.smoothscale(hacker_img, (200, 200))
-    hacker_b = screen.blit(hacker_img, [300, 300])
-
+    #hacker_b = screen.blit(hacker_img, [300, 300])
+    out:"hacker_img" = hacker_img
     # This depicts a guy whacking a projector with a sticker to represent someone teaching things.
-    training_img = pygame.image.load('resources/training.png')
+    training_img = pygame.image.load('dntos/ui/resources/training.png')
     training_img = pygame.transform.smoothscale(training_img, (200, 200))
-    training_b = screen.blit(training_img, [900, 300])
+    #training_b = screen.blit(training_img, [900, 300])
+    out:"training_img" = training_img
 
     # This is an image which contains all of the number 1. I miss CLI.
-    img_1 = pygame.image.load('resources/1.png')
+    img_1 = pygame.image.load('dntos/ui/resources/1.png')
     img_1 = pygame.transform.smoothscale(img_1, (100, 100))
-    img_1_b = screen.blit(img_1, [100, 100])
-
+    out:"img_1" = img_1
+    #img_1_b = screen.blit(img_1, [100, 100])
+    return(out)
 
 
 def Console(screen):
